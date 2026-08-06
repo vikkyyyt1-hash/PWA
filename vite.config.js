af400b81db6457736c8261daa74da09ebcefe3e9
Libero <vikkyyyt1@gmail.com>
2026-08-06 10:41:00 +0200

    Update_docs_with_relative_paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/track-s-main/',
+  base: './',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs',
